1d2c679461df7dd7280d0c54d7ff076f15ccad63
vmwebio <vmwebio@gmail.com>
2024-08-12 13:17:18 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <header class="header">
+    <h1>Rick and Morty API</h1>
+    <div class="filter"></div>
+  </header>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>

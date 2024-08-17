6e8c03dee3634cd3053b829f3b5e360703b21739
vmwebio <vmwebio@gmail.com>
2024-08-17 12:25:12 +0300

    feat: Настройка алиаса @

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
-// https://vitejs.dev/config/
+// Настройка алиаса '@'
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
   plugins: [vue()],
-})
+});
